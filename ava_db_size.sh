85ea7e949afb035678cacbb829db5bbaea3e707f
DougH <dougghernandez@gmail.com>
2021-02-16 04:59:28 +0000

    fix shebang error that was breaking all my scripts. Add units.

--- ava_db_size.sh
+++ ava_db_size.sh
@@ -1,7 +1,7 @@
-#~/bin/bash
+#!/bin/bash
 GetAvaDbSize=$(du -mx -d0 /home/ubuntu/.avalanchego/db | awk '{ print $1}')
 if [ -z "$GetAvaDbSize" ]
 then
 	return 1
 fi
-aws cloudwatch put-metric-data --metric-name AvaDbSize --namespace "AVAPRD01" --unit Megabytes --value $GetAvaDbSize --dimensions InstanceId=i-0f979461e744bfb81,InstanceType=t3a.medium
+aws cloudwatch put-metric-data --metric-name AvaDbSize --namespace "AVAPRD01" --value $GetAvaDbSize --dimensions InstanceId=i-0f979461e744bfb81,InstanceType=t3a.medium --unit "Megabytes"
--- check_ava_pid.sh
+++
@@ -1,8 +0,0 @@
-#~/bin/bash
-AVAPID=$(pgrep ava)
-ISALIVE=1
-if [ -z "$AVAPID" ]
-then
-	ISALIVE=0
-fi
-aws cloudwatch put-metric-data --metric-name is-ava-running --namespace "AVAPRD01" --value $ISALIVE --dimensions InstanceId=i-0f979461e744bfb81,InstanceType=t3a.medium 
