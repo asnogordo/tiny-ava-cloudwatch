348515ba18120f048ec68f25ed9ec86bbbb26d8a
DougH <dougghernandez@gmail.com>
2021-03-26 04:57:11 +0000

    various updates. change dbsize to use df instead of du

--- ava_db_size.sh
+++ ava_db_size.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-GetAvaDbSize=$(du -mx -d0 /home/ubuntu/.avalanchego/db | awk '{ print $1}')
+GetAvaDbSize=$(df -m /home/ | awk '{ print $3}' | sed 1d)
 if [ -z "$GetAvaDbSize" ]
 then
 	return 1
 fi
-aws cloudwatch put-metric-data --metric-name AvaDbSize --namespace "AVAPRD01" --value $GetAvaDbSize --dimensions InstanceId=i-0f979461e744bfb81,InstanceType=t3a.medium --unit "Megabytes"
+/usr/local/bin/aws cloudwatch put-metric-data --metric-name AvaDbSize --namespace "AVAPRD01" --value $(expr $GetAvaDbSize) --dimensions InstanceId=i-0f979461e744bfb81,InstanceType=t3a.medium --unit "Megabytes"
--- is_ava_running.sh
+++ is_ava_running.sh
@@ -5,4 +5,4 @@ if [ -z "$AVAPID" ]
 then
 	ISALIVE=0
 fi
-aws cloudwatch put-metric-data --metric-name is-ava-running --namespace "AVAPRD01" --value $ISALIVE --dimensions InstanceId=i-0f979461e744bfb81,InstanceType=t3a.medium --unit "Count"
+/usr/local/bin/aws cloudwatch put-metric-data --metric-name is-ava-running --namespace "AVAPRD01" --value $ISALIVE --dimensions InstanceId=i-0f979461e744bfb81,InstanceType=t3a.medium --unit "Count"
